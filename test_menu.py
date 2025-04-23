9afcd4c6e1c72d5083a06d2a97d3f657f5bb1bb2
donramones <agusrivarolaa00@gmail.com>
2025-04-23 18:50:00 -0300

    menu

---
+++ test_menu.py
@@ -0,0 +1,18 @@
+print(" Conversor y Operaciones Binarias ")
+print("1. Convertir Decimal a Binario")
+print("2. Convertir Binario a Decimal")
+#print("3. Sumar dos números binarios")
+#print("4. Restar dos números binarios")
+print("3. Salir")
+
+opcion = input("Seleccione una opción: ")
+
+if opcion =="1":
+    decimal = int(input("Ingrese el numero decimal: "))
+
+elif opcion=="2":
+    binario = input ("Ingrse un numero binario: ")
+elif opcion=="3":
+    print("Chau")
+else:
+    print("Opcion incorrecta,seleccione una opcion del menu")
